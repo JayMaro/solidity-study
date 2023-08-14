617bf3c00e51cc51dd55426647012baf3a19cc82
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-14 23:29:31 +0900

    Add: Frist.sol

---
+++ First.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier : MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Solidity {
+    // a -> Func() -> b
+    uint8 public a = 10;
+
+    // 10 -> Func() -> 15
+    function changeData() public {
+        a = 15;
+    }
+}
